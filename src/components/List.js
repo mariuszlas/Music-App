dba3c9be7fc735638404d53a34c65c1f173825bd
mariuszlas <mariusz.las@outlook.com>
2021-09-06 15:35:32 +0100

    Correcte syntax

--- src/components/List.js
+++ src/components/List.js
@@ -14,17 +14,17 @@ const List = () => {
             <tr>
                 <td>Sometimes I Sit and Think, and Sometimes I Just Sit</td>
                 <td> 20 March 2015</td>
-                <td><img src=""></td>
+                <td><img src=""/></td>
             </tr>
             <tr>
                 <td>Tell Me How You Really Feel</td>
                 <td>18 May 2018</td>
-                <td><img src=""></td>
+                <td><img src=""/></td>
             </tr>
             <tr>
                 <td>Things Take Time, Take Time</td>
                 <td>12 November 2021</td>
-                <td><img src=""></td>
+                <td><img src=""/></td>
             </tr>
         </table>
     );
