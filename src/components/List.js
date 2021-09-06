3f9a466f60644aca1577b2609e13fd4efc426b87
mariuszlas <mariusz.las@outlook.com>
2021-09-06 14:34:29 +0100

    Added List comopnent

---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrainerCard from './components/TrainerCard';
+import PokemonCards from './components/PokemonCards';
+
+function App() {
+    return (
+        <table>
+
+        </table>
+    );
+}
+
+export default App;
