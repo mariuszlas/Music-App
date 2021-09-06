48ed46a419e5caee1eb053c9a4a4fe7ba51945d7
mariuszlas <mariusz.las@outlook.com>
2021-09-06 17:29:59 +0100

    Added like buttons

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,20 @@
 import React from 'react';
-
+import { useState } from 'react';
 
 const List = () => {
 
+    const [status, setLikeStatus] = useState(false);
+
+    const handleLike = e => {
+        e.stopPropagation()
+        setLikeStatus(!status)
+        const row = e.target.closest('tr');
+        if (status) {
+            row.style.color = "red";
+        } else {
+            row.style.color = "black";        
+        }
+    }
 
     return (<table>
             <tr>
@@ -14,16 +26,19 @@ const List = () => {
                 <td>Sometimes I Sit and Think, and Sometimes I Just Sit</td>
                 <td> 20 March 2015</td>
                 <td><img src=""/></td>
+                <td><button onClick={handleLike}>Like it</button></td>
             </tr>
             <tr>
                 <td>Tell Me How You Really Feel</td>
                 <td>18 May 2018</td>
                 <td><img src=""/></td>
+                <td><button onClick={handleLike}>Like it</button></td>
             </tr>
             <tr>
                 <td>Things Take Time, Take Time</td>
                 <td>12 November 2021</td>
                 <td><img src=""/></td>
+                <td><button onClick={handleLike}>Like it</button></td>
             </tr>
         </table>
     );
