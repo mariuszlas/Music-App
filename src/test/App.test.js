2820040a4b53e9a10fd86b6aa9d4f05f44cad145
mariuszlas <mariusz.las@outlook.com>
2021-09-07 09:15:44 +0100

    Added a test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,1 +1,18 @@
 import App from '../App.js';
+import { screen, within } from '@testing-library/react';
+    {/* will select eg. <div role="feed"></div> */}
+
+
+describe('App', () => {
+
+    beforeEach(() => {
+        render(<App />)
+    })
+
+    test('buttons exist', () => {
+        const buttons = screen.getAllByRole('button');
+        expect(buttons.length).toBe(3);
+
+    })
+
+})
