a99a0260601e0f02b3de013d551a875946f99ff0
mariuszlas <mariusz.las@outlook.com>
2021-09-07 13:25:48 +0100

    Added List prop

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
+import { Artist } from '../../pages';
 
-const List = () => {
+const List = ( {albums} ) => {
 
     const [status, setLikeStatus] = useState(false);
 
@@ -12,7 +13,7 @@ const List = () => {
         if (status) {
             row.style.color = "red";
         } else {
-            row.style.color = "black";        
+            row.style.color = "black";
         }
     }
 
@@ -24,23 +25,24 @@ const List = () => {
                 <th>Album Cover</th>
             </tr>
             <tr>
-                <td>Sometimes I Sit and Think, and Sometimes I Just Sit</td>
-                <td> 20 March 2015</td>
+                <td>{albums[0].name}</td>
+                <td>{albums[0].date}</td>
                 <td><img src=""/></td>
                 <td><button onClick={handleLike}>Like it</button></td>
             </tr>
             <tr>
-                <td>Tell Me How You Really Feel</td>
-                <td>18 May 2018</td>
+                <td>{albums[1].name}</td>
+                <td>{albums[1].date}</td>
                 <td><img src=""/></td>
                 <td><button onClick={handleLike}>Like it</button></td>
             </tr>
             <tr>
-                <td>Things Take Time, Take Time</td>
-                <td>12 November 2021</td>
+                <td>{albums[2].name}</td>
+                <td>{albums[2].date}</td>
                 <td><img src=""/></td>
                 <td><button onClick={handleLike}>Like it</button></td>
             </tr>
+
             </tbody>
         </table>
     );
--- src/pages/Artist/index.js
+++ src/pages/Artist/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import { List, Intro } from '../../components'
+import { List, Intro } from '../../components';
+import { useState } from 'react';
 
 const Artist = () => {
+
+    const [ albums, setAlbums ] = useState([
+        {
+            name: 'Sometimes I Sit and Think, and Sometimes I Just Sit',
+            date: '20 March 2015'
+        },
+        {
+            name: 'Tell Me How You Really Feel',
+            date: '18 May 2018'
+        },
+        {
+            name: 'Things Take Time, Take Time',
+            date: '12 November 2021'
+        }
+    ])
+
     return (
       <main>
         <h1>Favourite Artist</h1>
         < Intro />
-        < List />
+        < List albums={albums}/>
       </main>
     );
   }
